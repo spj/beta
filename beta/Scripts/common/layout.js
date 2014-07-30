bbf64b7e49bf7edb92372a562231e8f39f5f6a9f
spj <shipj@hotmail.com>
2014-07-30 19:26:00 -0400

    --angular

--- beta/Scripts/common/layout.js
+++ beta/Scripts/common/layout.js
@@ -1,23 +1,31 @@
-﻿
-function loadUserAdmin() {
-    var _url = String.format("{0}{1}/GetView/{2}",beta.global.webroot, 'UsersAdmin', 'List');
-    var _container = '#main';
-    var _prefix = "UsersAdminListView"; _tmpl = _prefix + "Tmpl"; _modelName = _prefix + "Model";
-    $.when(
-    loadTemplate({ url: _url, template: _tmpl, container: _container, elementID: _prefix, modelName: _modelName, historyUrl: String.format("{0}UsersAdmin/List", beta.global.webroot) }),
-    $.getJSON(String.format("{0}GetDealerUsers/{1}",beta.global.webroot, beta.global.currentuser.dealer().DealerID))).done(function (model, users) {
-        model.users(users[0]);
-    });
-    ko.watch(beta.global.currentuser.dealer, function (parents, child, item) {
-        loadUserAdmin();
-    });
-}
-function loadRegister() {
-    var _url = String.format("{0}{1}/GetView/{2}",beta.global.webroot, 'Account', 'Register');
-    var _container = '#main';
-    var _prefix = "RegisterView"; _tmpl = _prefix + "Tmpl"; _modelName = _prefix + "Model";
-    loadTemplate({ url: _url, template: _tmpl, container: _container, element: _prefix, modelName: _modelName, historyUrl: String.format("{0}Register", beta.global.webroot) });
-}
+﻿$.ajaxSetup({ cache: true })
+angular.module('betaApp', ['ui.router', 'ui.bootstrap', 'bz.Directives'])
+.config(function ($stateProvider, $urlRouterProvider) {
+    // For any unmatched url, redirect to /state1
+    $urlRouterProvider.otherwise(beta.global.isAuthenticated ? "/home" : "/index");
+    // Now set up the states
+    $stateProvider
+      .state('index', {
+          url: "/index",
+          templateUrl: String.format("{0}{1}/GetView/{2}", beta.global.webroot, "Home", "Index")
+      })
+      .state('home', {
+          url: "/home",
+          templateUrl: String.format("{0}{1}/GetView/{2}", beta.global.webroot, "Home", "Home")
+      })
+      .state('register', {
+          url: "/register",
+          templateUrl: String.format("{0}{1}/GetView/{2}", beta.global.webroot, 'Account', 'Register'),
+          controller: function($scope,$http){
+              registerCtrl.call(this,$scope,$http);
+          }
+      })
+      .state('login', {
+          url: "/login",
+          templateUrl: String.format("{0}{1}/GetView/{2}", beta.global.webroot, 'Account', 'Login'),
+      })
+});
+
 
 function getUserDealers(user) {
     $.getJSON(String.format("{0}Independent/GetUserDealers", beta.global.webroot), { user: user }).done(function (data) {
@@ -32,26 +40,3 @@ function getUserDealers(user) {
     });
 
 }
-var _typeaheadObjects = [];
-var dealerTypeaheadHelper = function () {
-    return {
-        option: {
-            minLength: 4,
-            source: function (query, process) {
-                return $.getJSON(String.format("{0}Dealers/{1}",beta.global.webroot, query)).done(function (data) {
-                    _typeaheadObjects = data;
-                    process(getTypeAheadFromJson(data, 'Name'));
-                });
-            }
-            ,
-            updater: function (dealer) {
-                var _d = _(_typeaheadObjects).find(function (d) {
-                    return d.Name == dealer;
-                });
-                var _model = ko.dataFor($('#main form')[0]);
-                _model.dealerObj(_d);
-                return _d.Name;
-            }
-        }
-    };
-};
