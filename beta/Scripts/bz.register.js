511a0025c8969c13d744428a28592e4cb81f4f15
spj <shipj@hotmail.com>
2014-07-30 19:25:29 -0400

    --angular

--- beta/Scripts/bz.register.js
+++ beta/Scripts/bz.register.js
@@ -1,42 +1,21 @@
-﻿var RegisterViewModel = function () {
-    var _self = this;
-    this.omit = ["modelName", "dealerName", "dealerObj", "confirmPassword", "clearPassword"];
-    this.dealerName = korequire();
-    this.dealerObj = korequire();
-    this.dealer = koreset();
-    this.email = korequire().extend({ email: true });
-    this.phoneNumber = korequire().extend({ phoneUS: true });
-    this.userName = korequire().extend({required:true, pattern: '^[0-9a-zA-Z\ \]+$' });
-    this.clearPassword = korequire().extend({ passwordComplexity: true });
-    this.password = null;
-    this.confirmPassword = korequire().extend({
-        equal: { message: 'Passwords do not match.', params: this.clearPassword }
-    });
-    //errors set in dealerName.subscribe is reset
-    this.dealerNameValidator = function () {
-        _self.dealer(_self.dealerObj() && _self.dealerObj().Name == this.dealerName() ? _self.dealerObj().DealerID : null);
-        if (_self.dealer() == null)
-            ko.validation.setError(_self.dealerName, "Not found!");
-        else
-            ko.validation.clearError(_self.dealerName);
+﻿function registerCtrl($scope, $http) {
+    $scope.submitData = ["dealer", "userName", "email", "phoneNumber", "password"];
+    $scope.phoneChecker = function (value) {        
+        return null;
+    }
+    $scope.getDealers = function (val) {
+        return $http.get(String.format("{0}Dealers/{1}", beta.global.webroot, val)).then(function (res) {
+            return res.data;
+        });
     };
-    this.submit = function (form) {
-        this.errors = ko.validation.group(this);
-        if (this.isValid()) {
-            this.password = AESencrypt(this.clearPassword());
-            $.post(String.format("{0}Account/Register", beta.global.webroot), { data: submitData(this) }).done(function (data) {
-                if (data)
-                    showNotify(data);
-                else {
-                    $('#main').html("<strong>A verify code has been send to your email.</strong>");
-                    _self.reset();                   
-                }
-            });
-        } 
-    };
-    this.reset = function (data, event) {
-        resetViewModel(this, event);
-    };
+    $scope.submit = function () {
+        this.password = AESencrypt(this.clearPassword);
+        $.post(String.format("{0}Account/Register", beta.global.webroot), { data: submitData(this.data) }).done(function (data) {
 
-}
+        });
+    };
+    $scope.reset = function () {
+        $scope = {};
+    }
+};
 //# sourceURL=bz.register.js
