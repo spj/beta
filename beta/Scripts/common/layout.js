53ee69b40c405cafe5c84a82e9f58145d730ee8a
spj <shipj@hotmail.com>
2014-08-03 05:04:24 -0400

    user admin

--- beta/Scripts/common/layout.js
+++ beta/Scripts/common/layout.js
@@ -2,7 +2,10 @@
 angular.module('betaApp', ['ui.router', 'ui.bootstrap', 'bz.Directives'])
 .config(function ($stateProvider, $urlRouterProvider) {
     // For any unmatched url, redirect to /state1
-    $urlRouterProvider.otherwise(beta.global.isAuthenticated ? "/home" : "/index");
+   // $urlRouterProvider.otherwise(beta.global.isAuthenticated ? "/home" : "/index");
+    //$urlRouterProvider.otherwise(function ($injector, $location) {
+    //    return '/partials/contacts.' + '.html';
+    //});
     // Now set up the states
     $stateProvider
       .state('index', {
@@ -16,27 +19,49 @@ angular.module('betaApp', ['ui.router', 'ui.bootstrap', 'bz.Directives'])
       .state('register', {
           url: "/register",
           templateUrl: String.format("{0}{1}/GetView/{2}", beta.global.webroot, 'Account', 'Register'),
-          controller: function($scope,$http){
-              registerCtrl.call(this,$scope,$http);
+          controller: function ($scope, $http) {
+              registerCtrl.call(this, $scope, $http);
           }
       })
       .state('login', {
           url: "/login",
           templateUrl: String.format("{0}{1}/GetView/{2}", beta.global.webroot, 'Account', 'Login'),
       })
+      .state('roles', {
+          url: "^/roles",
+          templateUrl: String.format("{0}{1}/GetView/{2}", beta.global.webroot, 'RolesAdmin', 'Index'),
+      })
+    .state('users', {
+        url: "/users",
+        templateUrl: String.format("{0}{1}/GetView/{2}", beta.global.webroot, 'UsersAdmin', 'List'),
+        controller: function ($scope, $http) {
+            usersCtrl.call(this, $scope, $http);
+        }
+    })
+    .state('users.detail', {
+        url: "/detail/:uid",
+        templateUrl: String.format("{0}{1}/GetView/{2}", beta.global.webroot, 'UsersAdmin', 'Details'),
+        controller: function ($scope, $http, $stateParams) {
+            userCtrl.call(this, $scope, $http, $stateParams.uid);
+        }
+    })
+})
+.controller("accountCtrl", function ($scope, $http) {
+    $scope.getDealers = function (user) {
+        $.getJSON(String.format("{0}Independent/GetUserDealers", beta.global.webroot), { user: user }).done(function (data) {
+            $scope.$apply(function () {           
+                $scope.data = { email: user, fullname: data.UserFullName, dealer: data.Dealers[0], dealers: data.Dealers };
+            });
+            beta.global.currentuser = $scope.data;
+        })
+    };
+    $scope.change = function (dealer) {
+        $scope.data.dealer = dealer;
+        beta.global.currentuser.dealer = dealer;
+    }
+    $scope.getDealers(beta.global.currentuser.email);
+   
+        //$scope.$watch('beta.global.currentuser.email', function (newvalue, oldvalue) {
+
+        //});
 });
-
-
-function getUserDealers(user) {
-    $.getJSON(String.format("{0}Independent/GetUserDealers", beta.global.webroot), { user: user }).done(function (data) {
-        var _uobj = { email: user, fullname: data.UserFullName };
-        _uobj.dealer = ko.observable(data.Dealers[0]);
-        _uobj.dealers = data.Dealers;
-        beta.global.currentuser = _uobj;
-        beta.global.currentuser.change = function (dealer) {
-            beta.global.currentuser.dealer(dealer);
-        };
-        ko.applyBindings(beta.global.currentuser, $('#currentuser')[0]);
-    });
-
-}
