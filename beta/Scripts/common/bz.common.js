7e740374efa39d041ab19cfee3320947a9d6fdef
spj <shipj@hotmail.com>
2014-08-09 10:28:44 -0400

    empty

--- beta/Scripts/common/bz.common.js
+++ beta/Scripts/common/bz.common.js
@@ -1,16 +1,1 @@
 ﻿
-function loadView(controller, view) {
-    view = view || "index";
-    var _container = '#main';
-    var _url = String.format("{0}{1}/GetView/{2}",beta.global.webroot, controller, view);
-    loadTemplate({ url: _url, container: _container, historyUrl: String.format("{0}{1}/{2}",beta.global.webroot, controller, view) });
-}
-
-function showNotify(msg) {
-    $('.bottom-right').notify({
-        message: { text: msg }
-    }).show();
-}
-
-
-
