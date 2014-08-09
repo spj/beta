f1c7320359a0d4ca26271a421e4048785de29368
spj <shipj@hotmail.com>
2014-08-09 09:05:31 -0400

    --comment out ConfirmPassword

--- beta/Models/AccountViewModels.cs
+++ beta/Models/AccountViewModels.cs
@@ -73,10 +73,10 @@ namespace beta.Models {
         [Display(Name = "Password")]
         public string Password { get; set; }
 
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
+        //[DataType(DataType.Password)]
+        //[Display(Name = "Confirm password")]
        // [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
+        //public string ConfirmPassword { get; set; }
     }
 
     public class ResetPasswordViewModel {
